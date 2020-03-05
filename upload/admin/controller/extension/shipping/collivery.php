4b93088d9e01a34b7c8ccc1df1b429b3eac8d39e
fhulufhelo <fhulufhelo@mdsholdings.co.za>
2020-03-05 10:40:36 +0200

    move logic for add/remove table columns from xml to model when plugin is being installed or install

--- upload/admin/controller/extension/shipping/collivery.php
+++ upload/admin/controller/extension/shipping/collivery.php
@@ -7,11 +7,13 @@ class ControllerExtensionShippingCollivery extends Controller {
     }
 
     public function install() {
-
+        $this->load->model('extension/shipping/collivery');
+        $this->model_extension_shipping_collivery->addColumns();
     }
 
     public function uninstall() {
-
+        $this->load->model('extension/shipping/collivery');
+        $this->model_extension_shipping_collivery->dropColumns();
     }
 
 }
